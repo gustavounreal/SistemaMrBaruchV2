e8cc06e254127053d6709c6b704967911af74d09
Gustavo Macedo <gustavo.ribeiro.macedo@gmail.com>
2025-10-29 11:20:33 -0300

    atualização links urls dashborad central

--- sistemaMrBaruchProjeto/clientes/admin.py
+++ sistemaMrBaruchProjeto/clientes/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Cliente
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+	list_display = ('id', 'lead', 'data_nascimento', 'cidade', 'estado')
