0c4727490ce05d7d845b0630de03b29d774d3295
Gustavo Macedo <gustavo.ribeiro.macedo@gmail.com>
2025-10-30 15:36:14 -0300

    fix: Adiciona redirect de /captadores/ para /captadores/area/

--- sistemaMrBaruchProjeto/captadores/urls.py
+++ sistemaMrBaruchProjeto/captadores/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 app_name = 'captadores'
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='captadores:area_captador', permanent=False), name='index'),
     path('area/', views.area_captador, name='area_captador'),
 ]
